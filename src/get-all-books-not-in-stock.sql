7ddd00aa304013e84892788ab7a8413f9ce2ea9c
MariahCasciola417 <117303840+MariahCasciola@users.noreply.github.com>
2023-02-16 13:32:23 -0600

    queried get all books out of stock

--- src/get-all-books-not-in-stock.sql
+++ src/get-all-books-not-in-stock.sql
@@ -1,1 +1,7 @@
 -- Write a query to return a list of all book and author-related fields for all books that are currently out of stock.
+SELECT *
+FROM books b
+JOIN
+authors a
+ON a.author_id = b.author_id
+WHERE b.in_stock = 'false';
